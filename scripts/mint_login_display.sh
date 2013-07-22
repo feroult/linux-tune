a6f510532530ce4078786dd3620e30be302e53d3
Fernando Ultremare <feroult@gmail.com>
2013-07-21 23:48:39 -0300

    display

--- scripts/mint_login_display.sh
+++ scripts/mint_login_display.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+# http://www.gregfreeman.org/2012/fix-wrong-resolution-at-mdm-lightdm-login-screen-in-linux-mint-ubuntu/
+
 mode="$(xrandr -q|/bin/grep -A1 "VGA1 connected"| tail -1 |awk '{ print $1 }')"
 if [ -n "$mode" ]; then
   xrandr --output LVDS1 --off
